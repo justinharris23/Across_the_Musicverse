cf88f912a36f2ba0695c4409289253acad264767
Justin Harris <justin.r.harris@gmail.com>
2022-11-07 11:50:46 -0500

    album details working

--- across-the-musicverse/src/components/AlbumDetails.jsx
+++ across-the-musicverse/src/components/AlbumDetails.jsx
@@ -5,19 +5,28 @@ function AlbumDetails({ chart }) {
   let { id } = useParams();
 
   const [album, setAlbum] = useState(null);
-  console.log(chart);
 
   useEffect(() => {
-    let selectedAlbum = chart?.find((album) => album.id === id);
-    console.log(selectedAlbum);
-    setAlbum(selectedAlbum);
+    console.log(chart);
+    // let selectedAlbum = chart?.find((album) => album.id === id);
+    // console.log(selectedAlbum);
+    // setAlbum(selectedAlbum);
+
+    setAlbum(chart[0]);
+
+    console.log(album);
   }, []);
 
   return album ? (
     <div className="detail">
       <h1>{album.artist.name}</h1>
       <h2>{album.title}</h2>
-      <h2>{album.manufacturer}</h2>
+      <img
+        className="detailsImage"
+        src={album.cover_big}
+        width="250px"
+        height="250px"
+      />
     </div>
   ) : (
     <h1> album not found</h1>
--- across-the-musicverse/src/components/Header.jsx
+++ across-the-musicverse/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import Nav from "./Nav";
-import HeaderPicture from "./assets/music-header-int.jpg";
+import HeaderPicture from "./assets/music-header-new.jpg";
 
 export default function Header() {
   return (
@@ -7,12 +7,11 @@ export default function Header() {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link
-        className="headerImage"
         href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap"
         rel="stylesheet"
       />
       <h2 className="Main">Across the Musicverse</h2>
-      <img src={HeaderPicture} />
+      <img src={HeaderPicture} width="100%" height="auto" />
       <Nav />
     </div>
   );
